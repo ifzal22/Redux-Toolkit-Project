e97399ed8e3c1a8e5540bbeb238b9066ff5bf054
ifzal22 <enjoyyourlife2244@gmail.com>
2022-10-09 03:03:05 +0600

    video-feature

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import videosReducer from "../features/counter/video/videoSlice";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    videos: videosReducer,
   },
 });
